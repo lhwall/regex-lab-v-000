a5a421a297d46fb5407b1d27914d496b69a799b2
Lucy Wall <lhwall@gmail.com>
2018-03-27 01:17:20 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,33 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+if word.match(/\A[aeiouAEIOU]/) != nil
+  true 
+else false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/[^a-zA-Z]*un[a-zA-Z]*ing\s*/).collect do |i|
+ i.strip 
+end
 end
 
 def words_five_letters_long(text)
-
+ text.scan(/\b[a-zA-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ if text.match(/\A[A-Z].*[.?!]\z/) !=nil 
+   true 
+ else false
 end
+end 
 
 def valid_phone_number?(phone)
-
+if phone.match(/\d.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*/) != nil 
+  true 
+else false 
 end
+end
+
